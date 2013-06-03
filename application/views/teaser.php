31a3de47f2e6f599cd6150f0943559de82449944
Azhari Harahap <azhari@detik.com>
2013-06-03 11:11:11 +0700

    added bootstrap and teaser

---
+++ application/views/teaser.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Toothpaste</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="<?php echo base_url('media/bootstrap/css/bootstrap.min.css');?>" rel="stylesheet" media="screen">
+    </head>
+    <body>
+        <div class="container">
+            <h1></h1>
+            <blockquote>
+                <p>Welcome to <strong>Toothpaste</strong>, the Twitter #nowplaying</p>
+                <small>If you are waiting Twitter #music for too long, this is for you.</small>
+            </blockquote>
+            <ul class="nav nav-tabs nav-stacked">
+                <li><a href="#">#1 Pink feat Nate Ruess - Just Give Me A Reason</a></li>
+                <li><a href="#">#2 Justin Timberlake - Mirrors</a></li>
+                <li><a href="#">#3 Psy - Gentleman</a></li>
+                <li><a href="#">#4 Demi Lovato - Heart Attack</a></li>
+                <li><a href="#">#5 Pitbull feat Christina Aguilera - Feel This Moment</a></li>
+                <li><a href="#">#6 Macklemore &amp; Ryan Lewis feat Ray Dalton - Can't Hold Us</a></li>
+                <li><a href="#">#7 Will.i.am feat Justin Bieber - #ThatPower</a></li>
+                <li><a href="#">#8 Taylor Swift - 22</a></li>
+                <li><a href="#">#9 Bruno Mars - When I Was Your Man</a></li>
+                <li><a href="#">#10 One Direction - One Way Or Another (Teenage Kicks)</a></li>
+            </ul>
+            <p class="pull-right"><small><strong>Toothpaste</strong> is open source and you are welcome to contribute. Fork it on <a href="http://github.com/back2arie/toothpaste">Github.</a></small></p>
+        </div>
+    </body>
+</html>
